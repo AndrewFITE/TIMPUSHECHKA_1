48cdeddef0e6771c2196b9bb56e5c1dd7f117fe9
Андрей <145912299+AndrewFITE@users.noreply.github.com>
2023-11-14 06:01:55 +0300

    Update modAlphaCipher.cpp

--- Task2/modAlphaCipher.cpp
+++ Task2/modAlphaCipher.cpp
@@ -1,18 +1,18 @@
 #include "modAlphaCipher.h"
 using namespace std;
 
-wstring modAlphaCipher::coder(const wstring& open_st)
+wstring modAlphaCipher::coder(const wstring& open_row)
 {
-    wstring result = open_st;
+    wstring result = open_row;
     int length, number_of_rows, loc, a;
-    length = open_st.size();
+    length = open_row.size();
     number_of_rows = (length - 1)/newkey + 1;
     a = 0;
     for (int colum_number = newkey; colum_number > 0; colum_number--) {
         for (int line_number = 0; line_number < number_of_rows; line_number++) {
             loc = colum_number+newkey*line_number;
             if (loc-1 < length) {
-                result[a] = open_st[loc-1];
+                result[a] = open_row[loc-1];
                 a++;
             }
         }
@@ -20,18 +20,18 @@ wstring modAlphaCipher::coder(const wstring& open_st)
     return result;
 }
 
-wstring modAlphaCipher::decoder(const wstring& cipher_st)
+wstring modAlphaCipher::decoder(const wstring& cipher_row)
 {
-    wstring result = cipher_st;
+    wstring result = cipher_row;
     int length, number_of_rows, loc, a;
-    length = cipher_st.size();
+    length = cipher_row.size();
     number_of_rows = (length - 1)/newkey + 1;
     a = 0;
     for (int colum_number = newkey; colum_number > 0; colum_number--) {
         for (int line_number = 0; line_number < number_of_rows; line_number++) {
             loc = newkey*line_number+colum_number;
             if (loc-1 < length) {
-                result[loc-1] = cipher_st[a];
+                result[loc-1] = cipher_row[a];
                 a++;
             }
         }
